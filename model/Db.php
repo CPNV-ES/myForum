fcaa1a1069ebdebb7b722fe39c2f3849c8c55054
Dimitri Imfeld <dimfeld@protonmail.ch>
2020-10-02 14:58:50 +0200

    First iteration of Db utils class

---
+++ model/Db.php
@@ -0,0 +1,49 @@
+<?php
+
+class Db {
+    private static $instance;
+    private $dbConnection;
+
+    private function __construct() {
+        $creds = (require("../../myForum.credentials.php"))["mysql"];
+        $this->dbConnection = new PDO("mysql:host={$creds['host']};dbname={$creds['dbname']}", $creds["username"], $creds["passwd"]);
+    }
+
+    private function _getDbConnection() {
+        return $this->dbConnection;
+    }
+
+    /**
+     * Gets the current instance of Db
+     * @return Db The current instance
+     */
+    public static function getInstance() {
+        if(!isset(self::$instance)) {
+            self::$instance = new Db();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Returns the current open PDO connection to the database
+     * @return PDO The PDO connection
+     */
+    public static function getDbConnection() {
+        return self::getInstance()->_getDbConnection();
+    }
+
+    /**
+     * Executes the specified query with the specified parameters and returns the first row
+     * @param $query The SQL statement
+     * @param $params An array in which the keys are the parameter name and the value the parameter value
+     * @return mixed An array containing the selected rows, or false on error
+     */
+    public static function selectOneRecord($query, $params) {
+        $stmt = self::getInstance()->getDbConnection()->prepare($query);
+        foreach($params as $paramName => $paramVal) {
+            $stmt->bindParam(":" . $paramName, $paramVal);
+        }
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+}
