a0e57a80cd448e741b3bfe1ae964205ea732aade
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-10-02 10:34:43 +0200

    Add router

--- index.php
+++ index.php
@@ -2,13 +2,22 @@
 
 require 'vendor/autoload.php';
 
+require 'core/Renderer.php';
+
 use Expreql\Expreql\Database;
+use Routier\Routier\Router;
 
 $config = parse_ini_file('config.ini');
 
 Database::set_config($config);
+$router = new Router();
+$renderer = Renderer::get_instance();
 
+$router->get('/', function () {
+    
+});
 
+$router->execute();
 
 ?>
 
