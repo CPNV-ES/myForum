20de63adaff1ce49bc3390b4f36d642906a386e7
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-10-02 10:14:57 +0200

    add function selectOneRecord in the db file

--- model/db.php
+++ model/db.php
@@ -1,1 +1,11 @@
 <?php 
+
+/**
+ * This function try to select an entry in the database
+ * @param string $req 
+ * @param array $values
+ * @return array $entry
+ */
+function selectOneRecord($req,$values = null){
+
+}
