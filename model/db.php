36a81efcb53a33fff3eadeb11c2b3ef738cf5157
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-10-02 13:51:34 +0200

    refactor db.php to a class and replace initDbEnvironnement by the construtor

--- model/db.php
+++ model/db.php
@@ -6,37 +6,44 @@
  * Modified last : 2020-10-02
  **/
 
-$dbEnv = null;
-$dbConnection = null;
+class Db{
+    protected $dbEnv = null;
+    protected $dbConnection = null;
 
-/**
- * Load and initialize database environment variable
- */
-function dbEnvInit($envPath = '../env.json'){
-    $json = file_get_contents($envPath);
+    /**
+     * Load and initialize database environment variable
+     */
+    public function _construct($envPath = '../env.json'){
+        $json = file_get_contents($envPath);
 
-    $dbEnv = json_decode($json)["db"];
-    var_dump($dbEnv);
-}
+        $this->dbEnv = json_decode($json)["db"];
+        var_dump($this->dbEnv);
+    }
 
-/**
- * Initialize the connection with the database using environnement variable
- */
-function initDbConnection(){
-    if(empty($dbConnection)){
-        $dsn = 'mysql:host=' . $dbEnv["host"].';dbname='.$dbEnv["name"];
+    /**
+     * Initialize the connection with the database using environnement variable
+     */
+    function initDbConnection(){
+        if(empty($dbConnection)){
+            $dsn = 'mysql:host=' . $dbEnv["host"].';dbname='.$dbEnv["name"];
 
-        $dbConnection = new PDO($dsn,$dbEnv["login"]["username"],$dbEnv["login"]["password"]);
-    }    
-}
+            $dbConnection = new PDO($dsn,$dbEnv["login"]["username"],$dbEnv["login"]["password"]);
+        }    
+    }
 
 
-/**
- * This function try to select an entry in the database
- * @param string $req 
- * @param array $values
- * @return array $entry
- */
-function selectOneRecord($req,$values = null){
-    
+    /**
+     * This function try to select an entry in the database
+     * @param string $req 
+     * @param array $values
+     * @return array $entry
+     */
+    function selectOneRecord($req,$values = null){
+        initDbConnection();
+
+        $dbConnection->prepare($req);
+        $dbConnection->execute($values);
+
+        return $dbConnection->fetchAll();
+    }
 }
