bd95232174f9145684184854d53844bdd143c171
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-10-02 10:03:31 +0200

    add save function to the class role

--- model/Role.php
+++ model/Role.php
@@ -4,5 +4,10 @@ class Role{
     public $id;
     public $name;
 
-    
+    /**
+     * This method will insert in the database a new entry with current information
+     */
+    public function save(){
+        
+    }
 }
