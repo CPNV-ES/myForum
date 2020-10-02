8956ad197bb492926b19fedea512bc553d236a80
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-10-02 10:05:19 +0200

    add load function to the class role

--- model/Role.php
+++ model/Role.php
@@ -5,9 +5,16 @@ class Role{
     public $name;
 
     /**
-     * This method will insert in the database a new entry with current information
+     * This method will insert in the database a new entry with current information.
      */
     public function save(){
-        
+
+    }
+
+    /**
+     * This method try to load information in database based on properties informations.
+     */
+    public function load(){
+
     }
 }
