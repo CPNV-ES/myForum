062a80da6b59a8ea63b1eb2e5ab772e72e0ad129
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-02 13:53:23 +0200

    Preapre the configuration

--- model/db.php
+++ model/db.php
@@ -8,6 +8,13 @@ class DataBase
      */
     public function __construct()
     {
+        //Get the configuration from config.ini and put-it into variables.
+        $config  = parse_ini_file("config.ini");
+        $host    = $config[0];
+        $db      = $config[1];
+        $user    = $config[2];
+        $pass    = $config[3];
+        $charset = $config[4];
     }
 
     public function selectOneRecord($query, $value){
