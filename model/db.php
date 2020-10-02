59c027d88d1a4748ef230dff0cf5b1ca916801e9
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-10-02 10:37:42 +0200

    Add database connection, select and insert

--- model/db.php
+++ model/db.php
@@ -1,1 +1,26 @@
 <?php
+/**
+ * File : db.php
+ * Author : D. Ramos
+ * Created : 2020-10-02
+ * Modified last :
+ **/
+
+function databaseConnection() {
+    $connection = new PDO('mysql:host=localhost; dbname=myforum', 'root', '');
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $connection;
+}
+
+function selectOneRecord($request) {
+    $connection = databaseConnection();
+    $data = $connection->query($request);
+
+    return $data;
+}
+
+function insertOneRecord($request) {
+    $connection = databaseConnection();
+    $connection->exec($request);
+}
