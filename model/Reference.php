2012c11b4d3439ccaf8094df39e1365c6dd8c88f
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-02 10:28:44 +0200

    refactor Reference.php

--- model/Reference.php
+++ model/Reference.php
@@ -5,4 +5,37 @@
  * Modified last : 2020-10-02
  **/
 
-?>
+class Reference
+{
+    /**
+     * @var string
+     */
+    public $name;
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Reference constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function save()
+    {
+    }
+
+    public function load()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
