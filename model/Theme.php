137bab98f38ffb0c0f10369f676bc24f76e50a43
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-10-02 10:38:25 +0200

    Add empty functions

--- model/Theme.php
+++ model/Theme.php
@@ -1,7 +1,31 @@
 <?php
+/**
+ * File : db.php
+ * Author : D. Ramos
+ * Created : 2020-10-02
+ * Modified last :
+ **/
 
+require_once ("../db.php");
 
 class Theme
 {
+    public $id;
+    public $name;
 
+    public function save() {
+        insertOneRecord("insert into themes values ()", ["name" => "testing"]);
+    }
+
+    public function load() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
 }
