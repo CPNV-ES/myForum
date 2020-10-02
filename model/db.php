997d5b944c7f909b2b3467fb59f73ef9c1ecaaaa
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-10-02 13:33:00 +0200

    add the function to load the env files

--- model/db.php
+++ model/db.php
@@ -1,5 +1,16 @@
 <?php 
 
+$dbEnv = null;
+
+/**
+ * Load and initialize database environment variable
+ */
+function dbEnvInit($envPath = '../env.json'){
+    $json = file_get_contents($envPath);
+
+    $dbEnv = json_decode($json)["db"];
+}
+
 /**
  * This function try to select an entry in the database
  * @param string $req 
