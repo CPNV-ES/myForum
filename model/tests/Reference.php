dc0686c7ef0c581c8966d32b266592b77197e848
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-02 10:07:17 +0200

    Create Reference's class with atributes

---
+++ model/tests/Reference.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Reference
+{
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * Reference constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function save()
+    {
+    }
+}
