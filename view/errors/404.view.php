65e51d8efe8013e18148c123e6dfec38ee0b8e1b
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-10-07 17:14:21 +0200

    Basic navigation with MDBootstrap

--- view/errors/404.view.php
+++ view/errors/404.view.php
@@ -1,5 +1,5 @@
 <?php ob_start(); ?>
-<h1>404 - Page not found</h1>
+<h1 class="text-center p-5">404 - Page not found</h1>
 <?php
 $content = ob_get_clean();
 require_once "view/layout.view.php";
--- view/layout.view.php
+++ view/layout.view.php
@@ -3,11 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <title>myForum</title>
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="node_modules/mdbootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="node_modules/mdbootstrap/css/mdb.css">
+    <script src="node_modules/mdbootstrap/js/jquery.js"></script>
+    <script src="node_modules/mdbootstrap/js/bootstrap.js"></script>
+    <script src="node_modules/mdbootstrap/js/popper.js"></script>
 </head>
 <body>
-<div class="w-100 bg-primary font-weight-bolder text-center p-5"><h1>My Forum</h1></div>
-<?= $content ?>
+<div class="w-100 bg-primary font-weight-bolder p-5 navbar"><a class="text-reset" href="/"><h1>My Forum</h1></a></div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Utilisateur
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Alexandre</a>
+                <a class="dropdown-item" href="#">Andi</a>
+                <a class="dropdown-item" href="#">Cyril</a>
+                <a class="dropdown-item" href="#">Dimitri</a>
+                <a class="dropdown-item" href="#">Dylan</a>
+                <a class="dropdown-item" href="#">Gabriel</a>
+                <a class="dropdown-item" href="#">Mathieu</a>
+                <a class="dropdown-item" href="#">Quentin</a>
+                <a class="dropdown-item" href="#">Sou</a>
+                <a class="dropdown-item" href="#">William</a>
+                <a class="dropdown-item" href="#">Xavier</a>
+            </div>
+        </li>
+        <li class="nav-item"><a href="?controller=theme&action=index" class="btn">Gestion des thèmes</a></li>
+        <li class="nav-item"><a href="?controller=reference&action=index" class="btn">Gestion des références</a></li>
+        <li class="nav-item"><a href="?controller=role&action=index" class="btn">Gestion des rôles</a></li>
+        <li class="nav-item"><a href="?controller=state&action=index" class="btn">Gestion des états</a></li>
+    </ul>
+</nav>
+<div class="container-fluid">
+    <?= $content ?>
+</div>
 </body>
 </html>
