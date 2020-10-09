4c7518d0040ca8d06ef1eb867753fed4f1aae2b0
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-10-09 09:58:23 +0200

    Structure from master

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+require_once "controller/ReferenceController.php";
+require_once "controller/RoleController.php";
+require_once "controller/StateController.php";
+require_once "controller/ThemeController.php";
+
+$controller = $_GET['controller'] . "Controller";
+$action = $_GET['action'];
+$id = intval($_GET['id']);
+if (empty($controller) || empty($action)) {
+    require_once "view/homepage.view.php";
+} else {
+    if (class_exists($controller)) {
+        $ctr = new $controller();
+        if (method_exists($ctr, $action)) {
+            if ($id) {
+                $ctr->$action($id);
+            } else {
+                $ctr->$action();
+            }
+        } else {
+            require_once "view/errors/404.view.php";
+        }
+    } else {
+        require_once "view/errors/404.view.php";
+    }
+}
+?>
+
+
