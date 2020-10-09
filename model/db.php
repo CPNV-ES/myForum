58ceb00365389473db9f0dd175cc9fa493b183a0
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-09 08:46:57 +0200

    Adding "BindParam"'s function

--- model/db.php
+++ model/db.php
@@ -15,18 +15,14 @@ class DataBase
         $this -> dbo = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['db'], $config['user'], $config['pass']);
     }
 
-    public function selectOneRecord($query, $name){
-        $query = "select * from myforum.references where id = 1";
+    public function selectOneRecord($query, $id){
         $sth = $this -> dbo->prepare($query);
-        //$sth = $this -> $dbo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-
+        $sth->bindParam(':id', $id, PDO::PARAM_INT);
         $sth->execute();
-        $datas = $sth->fetchAll();
 
+        $datas = $sth->fetchAll();
         print_r($datas);
-        /*
-        $stmt->bind_param($name, $query);
-        $stmt->execute();*/
+
         return 1;
     }
 }
