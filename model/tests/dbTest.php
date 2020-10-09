d987b3351df0d065361234dd74b5f2b6c236395e
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-09 10:08:36 +0200

    Update dbTest.php

--- model/tests/dbTest.php
+++ model/tests/dbTest.php
@@ -6,7 +6,7 @@ $query = "select * from myforum.references where description = :desc";
 $values = ["desc" => "Scrum guide"];
 
 $db = new DataBase();
-$datas = $db->selectOneRecord($query, $values
+$datas = $db->selectOneRecord($query, $values);
 var_dump($datas);
 
 
