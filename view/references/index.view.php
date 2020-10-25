b93f042bb7d6932c213379d430b144271be4e94e
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-10-25 15:03:56 +0100

    Simple list of references

--- view/references/index.view.php
+++ view/references/index.view.php
@@ -1,5 +1,13 @@
 <?php ob_start(); ?>
-<h1 class="text-center p-5">List Reference</h1>
+<div class="container">
+    <h1 class="text-center p-5">Liste des réferences</h1>
+    <?php foreach ($references as $reference) { ?>
+        <div class="row text-center">
+            <a href="<?= $reference->url ?>"><?= $reference->description ?></a>
+        </div>
+    <?php } ?>
+</div>
+
 <?php
 $content = ob_get_clean();
 require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
