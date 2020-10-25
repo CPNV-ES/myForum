0f08ebd0d9b6d2287b84f421cf2946466de494e6
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-10-25 16:44:16 +0100

    Cosmetics

--- view/references/index.view.php
+++ view/references/index.view.php
@@ -1,22 +1,20 @@
 <script src="view/references/referencelist.js" defer></script>
 
 <?php ob_start(); ?>
-<div class="container">
-    <h1 class="text-center p-5">Liste des réferences</h1>
-    <?php foreach ($references as $reference) { ?>
-        <div id="ref<?= $reference->url ?>" data-id="<?= $reference->id ?>" class="row text-center divtitle">
-            <div class="col-2 text-right">
-                <div id="divIcons<?= $reference->id ?>" class="d-none">
-                    <a class="blacklink m-2" href="<?= $reference->url ?>"><i class="fas fa-eye"></i></a>
-                    <a class="blacklink m-2" href="?controller=reference&action=edit&id=<?= $reference->id ?>"><i class="fas fa-edit"></i></a>
-                </div>
-            </div>
-            <div class="col-10 text-left">
-                <?= $reference->description ?>
+<h1 class="text-center p-5">Liste des réferences</h1>
+<?php foreach ($references as $reference) { ?>
+    <div id="ref<?= $reference->url ?>" data-id="<?= $reference->id ?>" class="row text-center divtitle">
+        <div class="col-2 text-right">
+            <div id="divIcons<?= $reference->id ?>" class="d-none">
+                <a class="blacklink m-2" href="<?= $reference->url ?>"><i class="fas fa-eye"></i></a>
+                <a class="blacklink m-2" href="?controller=reference&action=show&id=<?= $reference->id ?>"><i class="fas fa-search-plus"></i></a>
             </div>
         </div>
-    <?php } ?>
-</div>
+        <div class="col-10 text-left">
+            <?= $reference->description ?>
+        </div>
+    </div>
+<?php } ?>
 
 <?php
 $content = ob_get_clean();
