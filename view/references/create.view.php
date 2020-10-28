a5b2b7cf9605b9e4b1b929398093bd635c691e6c
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-10-28 17:07:39 +0100

    Refactor: same form body for update and create

--- view/references/create.view.php
+++ view/references/create.view.php
@@ -1,13 +1,6 @@
 <?php ob_start(); ?>
 <form action="?controller=Reference&action=store" method="post">
-    <div class="row">
-        <div class="bg-light col-2 border border-dark">Titre:</div>
-        <input type="text" name="description" class="col-10" value="<?= $reference->description ?>" />
-    </div>
-    <div class="row">
-        <div class="bg-light col-2 border border-dark">URL:</div>
-        <input type="url" name="url" class="col-10" value="<?= $reference->url ?>" />
-    </div>
+    <?php require ("form_body.view.php") ?>
     <div class="row m-3">
         <button class="btn btn-success">Enregistrer</button>
         <a href="?controller=Reference&action=show&id=<?= $reference->id ?>" class="btn btn-primary">Annuler</a>
---
+++ view/references/form_body.view.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="bg-light col-2 border border-dark">Titre:</div>
+    <input type="text" name="description" class="col-10" value="<?= $reference->description ?>" />
+</div>
+<div class="row">
+    <div class="bg-light col-2 border border-dark">URL:</div>
+    <input type="url" name="url" class="col-10" value="<?= $reference->url ?>" />
+</div>
