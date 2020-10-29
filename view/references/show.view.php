ae95798b5bf10eaf06cb6a153158320fffb4b1ee
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-10-29 13:07:15 +0100

    Confirmation popup on delete

--- view/references/show.view.php
+++ view/references/show.view.php
@@ -8,8 +8,21 @@
     <div class="col-10"><?= $reference->url ?></div>
 </div>
 <div class="row m-3">
-    <a href="?controller=Reference&action=destroy&id=<?= $reference->id ?>" class="btn btn-danger">Supprimer</a>
+    <a href="#" data-toggle="modal" data-target="#confirm-delete" class="btn btn-danger">Supprimer</a>
     <a href="?controller=Reference&action=edit&id=<?= $reference->id ?>" class="btn btn-primary">Modifier</a>
+</div>
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body text-center">
+                Sûr et certain ???
+            </div>
+            <div class="modal-footer flex-row justify-content-center">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                <a class="btn btn-danger btn-ok" href="?controller=Reference&action=destroy&id=<?= $reference->id ?>">Confirmer</a>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
 $content = ob_get_clean();
