421e59a6d309650f1dcb49d72ad009580e45ec90
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-10-29 10:42:54 +0100

    Added isset($id)

--- index.php
+++ index.php
@@ -8,14 +8,16 @@ require_once "controller/ThemeController.php";
 
 $controller = $_GET['controller'] . "Controller";
 $action = $_GET['action'];
-$id = intval($_GET['id']);
+if(isset($_GET['id'])){
+    $id = intval($_GET['id']);
+}
 if (empty($controller) || empty($action)) {
     require_once "view/homepage.view.php";
 } else {
     if (class_exists($controller)) {
         $ctr = new $controller();
         if (method_exists($ctr, $action)) {
-            if ($id) {
+            if (isset($id)) {
                 $ctr->$action($id);
             } else {
                 $ctr->$action();
