830d7944b5611dafdb995e2896b7bb240f82d0cb
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-30 10:37:09 +0100

    [with bugs] Displaying themes

--- view/themes/index.view.php
+++ view/themes/index.view.php
@@ -1,6 +1,17 @@
-<?php ob_start(); ?>
+<?php 
+    ob_start(); 
+?>
+
 <h1 class="text-center p-5">List Theme</h1>
+<h2> Displaying themes list </h2>
+
 <?php
 $content = ob_get_clean();
+
+//require('../../model/Theme.php');
+
+//$t = new Theme;
+
 require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
+
 ?>
