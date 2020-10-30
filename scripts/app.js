6891b2b821a1ffae701c942325fbeef9a91f0bd9
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-10-30 09:44:46 +0100

    Register views

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,20 @@
 import Router from './router.js';
 
 import HomeView from './views/home.js';
+import ReferencesView from './views/references.js';
+import RolesView from './views/roles.js';
+import StatesView from './views/states.js';
+import ThemesView from './views/themes.js';
 
 let routerContainer = document.getElementById('content');
 let router = new Router(routerContainer);
 
 router.routes([
     { path: '/', component: HomeView },
+    { path: '/references', component: ReferencesView },
+    { path: '/themes', component: ThemesView },
+    { path: '/roles', component: RolesView },
+    { path: '/states', component: StatesView },
 ]);
 
 router.execute();
