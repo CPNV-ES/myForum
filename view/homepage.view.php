99695daaadc0e5ea9ea0b498865b5c471fa3f605
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-10-30 10:03:11 +0100

    Update homepage.view.php

--- view/homepage.view.php
+++ view/homepage.view.php
@@ -1,6 +1,10 @@
-<?php ob_start(); ?>
-<h1 class="text-center p-5">Home</h1>
 <?php
-$content = ob_get_clean();
-require_once "layout.view.php";
+    ob_start();
+?>
+
+<h1 class="text-center p-5">Home</h1>
+
+<?php
+    $content = ob_get_clean();
+    require_once "layout.view.php";
 ?>
