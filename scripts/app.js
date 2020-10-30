24cb151e4ce9ac14ffccf6a16dcd90567ba3dd67
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-10-30 09:32:21 +0100

    Remove topic view

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,12 @@
 import Router from './router.js';
 
 import HomeView from './views/home.js';
-import TopicView from './views/topic.js';
 
 let routerContainer = document.getElementById('content');
 let router = new Router(routerContainer);
 
 router.routes([
     { path: '/', component: HomeView },
-    { path: '/topic', component: TopicView }
 ]);
 
 router.execute();
--- scripts/views/topic.js
+++
@@ -1,9 +0,0 @@
-export default class Topic {
-
-    render() {
-        return `
-            <div>topic</div>
-            <a href="/">home</a>
-        `
-    }
-}
