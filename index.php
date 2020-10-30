e7439acbe509de790d2f9973854627cbc3c5c5c7
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-10-30 09:26:57 +0100

    Update header to boostrap styles

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myForum</title>
     <script src="scripts/app.js" type="module"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css"></script>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    </script>
     <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
-    <header class="top-bar">
-        <div class="top-bar-content">
-            <div class="top-bar-title">MyForum</div>
-            <div class="search-bar">
-                <input type="search" placeholder="Rechercher">
+    <nav class="navbar navbar-light bg-light px-3 border-top border-red border-4">
+        <div class="row">
+            <div class="col">
+                <a class="navbar-brand" href="/">MyForum</a>
+
             </div>
         </div>
-    </header>
+        <div class="row">
+            <div class="col">
+                <a class="btn btn-primary" href="/themes">Gestion des thèmes</a>
+                <a class="btn btn-primary" href="/refereneces">Gestion des références</a>
+                <a class="btn btn-primary" href="/roles">Gestion des rôles</a>
+                <a class="btn btn-primary" href="/states">Gestion des états</a>
+            </div>
+        </div>
+    </nav>
     <div id="content"></div>
 </body>
+
 </html>
