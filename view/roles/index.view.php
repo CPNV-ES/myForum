a7ce7c2681697ac4247cd1c15149a46cb8c41ef1
Dimitri Imfeld <dimfeld@protonmail.ch>
2020-10-30 09:30:25 +0100

    Update role index page, add links to show, update and add pages

--- view/roles/index.view.php
+++ view/roles/index.view.php
@@ -5,28 +5,32 @@ $content = ob_get_clean();
 require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
 ?>
 <div class="container">
-<table class='table table-bordered table-striped table-sm col col-12'>
-    <thead class="blue-gradient-rgba white-text">
+<table class='table table-borderless table-sm col col-12'>
+    <thead>
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">name</th>
+            <th scope="col"></th>
+            <th scope="col" class="blue-gradient-rgba white-text">Nom</th>
         </tr>
     </thead>
     <tbody>
 
 <?php
 foreach($roles as $role) {
-    echo "<tr>";
-    echo "<th scope='row'>" . $role->id . "</th>";
-    echo "<td>";
-    echo "<a href='?controller=role&action=show&id=" . $role->id . "'>";
-    echo $role->name;
-    echo "</td>";
-    echo "</a>";
-    echo "</tr>";
+    echo <<<EOD
+    <tr>
+        <th class="col-md-1 col-icons">
+            <a href="?controller=role&action=show&id=$role->id"><i class="fas fa-eye"></i></a>
+            <a href="?controller=role&action=update&id=$role->id"><i class="fas edit"></i></a>
+        </th>
+        <td class="th-lg">
+            $role->name
+        </td>
+    </tr>
+EOD;
 }
 ?>
 
     </tbody>
 </table>
+<a href="/?controller=role&action=create"><button type="button" class="btn btn-primary">Nouveau</button></a>
 </div>
