1c835f293f10ae125e33041dce166c50307fb785
Alexandre Philibert <=>
2020-11-06 09:14:08 +0100

    Fix relative path of hyperlinks

--- layout.php
+++ layout.php
@@ -8,9 +8,9 @@ $renderer = Renderer::get_instance();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>myForum</title>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/style.css">
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/style.css">
 </head>
 
 <body>
