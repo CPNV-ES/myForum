be90c61747bd3cb0b1cc582a3349ce9b4a9bc28b
Dimitri Imfeld <dimfeld@protonmail.ch>
2020-11-06 09:19:15 +0100

    Reference index page

--- view/references/index.view.php
+++ view/references/index.view.php
@@ -4,3 +4,34 @@
 $content = ob_get_clean();
 require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
 ?>
+<div class="container">
+<table class='table table-borderless table-sm col col-12'>
+    <thead>
+        <tr>
+            <th scope="col"></th>
+            <th scope="col" class="blue-gradient-rgba white-text">Description</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php
+foreach($references as $reference) {
+    echo <<<EOD
+    <tr>
+        <th class="col-md-1 col-icons">
+            <a href="$reference->url" target="_blank"><i class="fas fa-link"></i></a>
+            <a href="?controller=reference&action=show&id=$reference->id"><i class="fas fa-eye"></i></a>
+            <a href="?controller=reference&action=edit&id=$reference->id"><i class="fas fa-edit"></i></a>
+        </th>
+        <td class="th-lg">
+            $reference->description
+        </td>
+    </tr>
+EOD;
+}
+?>
+
+    </tbody>
+</table>
+<a href="/?controller=reference&action=create"><button type="button" class="btn btn-primary">Nouveau</button></a>
+</div>
