3b706732ac163d78e2176e0815d7ddb37e7822e1
rossier gabriel <rossier.gabriel218@gmail.com>
2020-11-06 09:24:05 +0100

    Closing td

--- view/references/index.view.php
+++ view/references/index.view.php
@@ -1,50 +1,52 @@
-
 <!--Falsh message (Message stocker dans la question)-->
 <?php ob_start(); ?>
 <h1 class="text-center p-5">List Reference</h1>
 
 
-<?php 
-    if(isset($_SESSION['flashMessage']))
-    {
-        echo "<div class='alert alert-primary'>";
-        echo $_SESSION['flashMessage'];
-        unset($_SESSION['flashMessage']);
-        echo "</div>";
-    }
-    
+<?php
+if (isset($_SESSION['flashMessage'])) {
+    echo "<div class='alert alert-primary'>";
+    echo $_SESSION['flashMessage'];
+    unset($_SESSION['flashMessage']);
+    echo "</div>";
+}
+
 ?>
 </div>
 <a class="btn btn-primary" href="?controller=reference&action=create">Create</a>
-<table class="table">
-<?php
+<table id="references" class="table">
+    <thead>
+        <tr>
+            <th>
+                <h4>Description</h4>
+            </th>
+            <th>
+                <h4>URL</h4>
+            </th>
+            <th>
+                <h4>Action</h4>
+            </th>
+        </tr>
+    </thead>
+    <?php
 
-foreach ($references as $Key => $Value) : ?>
-
-    
-
-        <thead>
-            <tr>
-                <th>Description</th>
-                <th>URL</th>
-                <th>Action</th>
-            </tr>
-        </thead>
+    foreach ($references as $Key => $Value) : ?>
         <tbody>
             <tr>
                 <td>
                     <?= $Value->description ?>
                 </td>
-                <td><a href="<?=$Value->url?>"><?= $Value->url ?></a></td>
+                <td><a href="<?= $Value->url ?>"><?= $Value->url ?></a></td>
                 <td>
                     <a class="btn btn-primary" href="?controller=Reference&action=show&id=<?= $Value->id ?>">Details</a>
+                </td>
             </tr>
         </tbody>
-   
 
 
-      
-<?php endforeach; ?>
+
+
+    <?php endforeach; ?>
 </table>
 <?php
 
