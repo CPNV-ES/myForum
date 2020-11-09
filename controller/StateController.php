bd7fe04fcb8c7864a60807d3ace8bd96aa771bde
Math <math.oron@gmail.com>
2020-11-09 21:43:54 +0100

    Adding the functions edit and update into the state section

--- controller/StateController.php
+++ controller/StateController.php
@@ -31,12 +31,49 @@ class StateController
 
     public function edit($id) // simply show the edit form
     {
+        $state = new State();
+        $state->id = $id;
+        $state->load();
+
         require_once $_SERVER['DOCUMENT_ROOT']."/view/states/update.view.php";
     }
 
     public function update($id) // handle edit form submit
     {
-        require_once $_SERVER['DOCUMENT_ROOT']."/view/states/show.view.php"; // back to show after saving changes
+        $showErrMsg = function() {
+            global $id;
+            ViewHelpers::pushFlashMessage([
+                "text" => "Erreur lors de la mise à jour du rôle avec l'id '{$id}' ",
+                "type" => "error"
+            ]);
+            header("Location: /?controller=state&action=index");
+            return;
+        };
+
+        $state = new State();
+        $state->id = $id;
+        $state->load();
+
+        if($state->id == null || !isset($_GET["name"])) {
+            $showErrMsg();
+            return;
+        }
+
+        $state->name = htmlspecialchars($_GET["name"]);
+        $success = $state->update();
+
+        if($success) {
+            ViewHelpers::pushFlashMessage([
+                "text" => "Le rôle '{$state->name}' a été mis à jour",
+                "type" => "info"
+            ]);
+        }
+        else {
+            $showErrMsg();
+            return;
+        }
+
+        header("Location: /?controller=state&action=index");
     }
 
     public function destroy($id)
