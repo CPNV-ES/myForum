f26e9e69087b9eb88ac2a785f9e7e39e55ccc15d
Dimitri Imfeld <dimfeld@protonmail.ch>
2020-11-10 10:54:24 +0100

    Implement load() for Opinion model

--- docs/journal.md
+++ docs/journal.md
@@ -8,4 +8,5 @@
 # Execution
 - Mise en place de la nouvelle BDD
 - Mise en place de la structure (modèle, vue, controlleur) vide pour l'instant afin de permettre d'afficher une simple page vide pour la modération des opinions
--
+- Implémentation de nouvelles methodes DB (selectToArray) vu que select() parse lui-même le résultat de la query et que la table Opinions a des clés étrangères ... 
+- Implémentation de la méthode load() du modèle Opinion
--- model/Opinion.php
+++ model/Opinion.php
@@ -3,5 +3,51 @@
 require_once("Db.php");
 
 class Opinion {
+    public $id;
+    public $description;
+    public $state;
+    public $user_pseudo;
 
+    /**
+     * Returns an array of objects representing all records of the table
+     * @returns array<Reference> An array containing instances of all the References stored in the database
+     */
+    public static function all() {
+
+    }
+
+    /**
+     * Load data from the database based on this instance's id property
+     * @return bool true on success, false otherwise
+     */
+    public function load() {
+        if ($this->id == null)
+            return false;
+
+        Db::getDbConnection()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $record = Db::selectOneToArray("
+            SELECT description as description, users.pseudo as user_pseudo, opinionstates.name as opinionstate_name
+            FROM `opinions` 
+            INNER JOIN users ON users.id = opinions.user_id
+            INNER JOIN opinionstates on opinionstates.id = opinionstate_id
+            WHERE `opinions`.`id`=:id",
+            ["id" => $this->id]);
+        
+
+        if ($record) {
+            $this->description = $record["description"];
+            $this->user_pseudo = $record["user_pseudo"];
+            $this->state = $record["opinionstate_name"];
+
+            return true;
+        } else {
+            $this->id = null;
+            $this->description = null;
+            $this->user_pseudo = null;
+            $this->state = null;
+
+            return false;
+        }
+    }
 }
