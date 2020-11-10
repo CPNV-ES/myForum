9069902d4047622b394f3282b1f90256ec707e0f
Dimitri Imfeld <dimfeld@protonmail.ch>
2020-11-10 11:52:45 +0100

    Switch different text elements from english to french

--- view/opinions/moderate.view.php
+++ view/opinions/moderate.view.php
@@ -1,5 +1,5 @@
 <?php ob_start(); ?>
-<h1 class="text-center p-5">Moderate Opinions</h1>
+<h1 class="text-center p-5">Opinions</h1>
 <?php
 $content = ob_get_clean();
 require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
@@ -21,9 +21,9 @@ require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
 <table class='table table-borderless table-sm col col-12'>
     <thead>
         <tr class="blue-gradient-rgba white-text">
-            <th>Author</th>
+            <th>Autheur</th>
             <th>Opinion</th>
-            <th>State</th>
+            <th>Etat</th>
         </tr>
     </thead>
     <tbody>
