1776ad5e783741bab852f063ee5b1e4a2fc096a2
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-11-10 11:56:27 +0100

    Add attributes for filtering in moderaiton view

--- views/moderation.php
+++ views/moderation.php
@@ -3,19 +3,19 @@
 <div class="row justify-content-end">
     <div class="col col-2">
         <div>Filtre: </div>
-        <select class="form-control " name="filter" id="filter">
+        <select class="form-control" name="filter" id="filter" data-filter="opinion-table" data-filter-attr="data-state-id">
             <option value="all">--- Tous ---</option>
             <?php foreach ($opinion_states as $state) : ?>
-                <option value="<?= $state->id?>"><?= $state->name ?></option>
+                <option value="<?= $state->id ?>"><?= $state->name ?></option>
             <?php endforeach; ?>
         </select>
     </div>
 </div>
 
-<table class="table">
+<table id="opinion-table" class="table">
     <tbody>
         <?php foreach ($opinions as $opinion) : ?>
-            <tr>
+            <tr data-state-id="<?= $opinion->opinionstates[0]->id ?>">
                 <td><?= $opinion->users[0]->pseudo ?></td>
                 <td><?= $opinion->description ?></td>
                 <td><?= $opinion->opinionstates[0]->name ?></td>
