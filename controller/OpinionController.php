d14574edfc1ec9e0b46aaf5f5f0d125245c6dee9
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-11-10 10:26:05 +0100

    Have an opinions controller

---
+++ controller/OpinionController.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "model/Opinion.php";
+
+class OpinionController
+{
+    public function index()
+    {
+        $opinions = Opinion::all();
+        require_once $_SERVER['DOCUMENT_ROOT']."/view/roles/index.view.php";
+    }
+}
