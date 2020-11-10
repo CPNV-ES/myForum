980a5d43e9dfd9177d48cf945aebbdaea9e487dc
SANTOS-OLIVEIRA Andi <Andi.SANTOS-OLIVEIRA@cpnv.ch>
2020-11-10 11:11:51 +0100

    Resolve bootstrap.js doesn't find popper.js

--- view/layout.view.php
+++ view/layout.view.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.css">
     <link rel="stylesheet" href="css/all.css">
     <script src="node_modules/mdbootstrap/js/jquery.js"></script>
-    <script src="node_modules/mdbootstrap/js/bootstrap.js"></script>
     <script src="node_modules/mdbootstrap/js/popper.js"></script>
+    <script src="node_modules/mdbootstrap/js/bootstrap.js"></script>
 </head>
 <body>
 <div class="w-100 bg-primary font-weight-bolder p-5 navbar"><a class="text-reset" href="/"><h1>My Forum</h1></a></div>
