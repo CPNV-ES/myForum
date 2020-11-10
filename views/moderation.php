d55c7c7b3876481c33149dfd68b41e1a7fb6e333
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2020-11-10 10:27:11 +0100

    Add moderation view

---
+++ views/moderation.php
@@ -0,0 +1,15 @@
+<h1>Opinions</h1>
+
+<div class="row justify-content-end">
+    <div class="col col-2">
+        <div>Filtre: </div>
+        <select class="form-control " name="filter" id="filter">
+            <option value="all">--- Tous ---</option>
+        </select>
+    </div>
+</div>
+
+<table>
+    <tbody>
+    </tbody>
+</table>
