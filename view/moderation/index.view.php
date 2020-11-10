5eb59293f9aba560c8dbcdde2febe10398a1aa4d
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-11-10 10:26:33 +0100

    add moderation index

--- view/moderation/index.view.php
+++ view/moderation/index.view.php
@@ -1,5 +1,5 @@
 <?php ob_start(); ?>
-<h1 class="text-center p-5">List Reference</h1>
+<h1 class="text-center p-5">Modération</h1>
 <?php
 $content = ob_get_clean();
 require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
@@ -8,23 +8,23 @@ require_once $_SERVER['DOCUMENT_ROOT']."/view/layout.view.php";
 <table class='table table-borderless table-sm col col-12'>
     <thead>
         <tr>
-            <th class="blue-gradient-rgba white-text">Description</th>
+            <th class="blue-gradient-rgba white-text">Opinions</th>
             <th scope="col" class="description-title"></th>
         </tr>
     </thead>
     <tbody>
 
 <?php
-foreach($references as $reference) {
+foreach($opinions as $opinion) {
     echo <<<EOD
     <tr>
         <th>
-            $reference->description
+            $opinion->description
         </th>
         <td scope="col" class="col-icons">
-            <a href="$reference->url" target="_blank"><i class="fas fa-link"></i></a>
-            <a href="?controller=reference&action=show&id=$reference->id"><i class="fas fa-eye"></i></a>
-            <a href="?controller=reference&action=edit&id=$reference->id"><i class="fas fa-edit"></i></a>
+            <a href="$opinion->url" target="_blank"><i class="fas fa-link"></i></a>
+            <a href="?controller=opinion&action=show&id=$opinion->id"><i class="fas fa-eye"></i></a>
+            <a href="?controller=opinion&action=edit&id=$opinion->id"><i class="fas fa-edit"></i></a>
         </td>
     </tr>
 EOD;
@@ -33,5 +33,5 @@ EOD;
 
     </tbody>
 </table>
-<a href="/?controller=reference&action=create"><button type="button" class="btn btn-primary">Nouveau</button></a>
+<a href="/?controller=opinion&action=create"><button type="button" class="btn btn-primary">Nouveau</button></a>
 </div>
