8c5e9ca780b2a222fde7e80613c7a526a01934fa
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-11-10 09:25:10 +0100

    Remove redundant javascript references script

--- view/references/references.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('tr').mouseenter(function () {
-        $(this).find('span').removeClass('hidden-element');
-    }).mouseleave(function () {
-        $(this).find('span').addClass('hidden-element');
-    })
-});
