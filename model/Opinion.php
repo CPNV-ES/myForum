7ea94fdb76eed83ed2361de37f8e0ecb244d7e7b
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-11-10 10:42:38 +0100

    add the method load to get the information about the opinion from the database to set it to the object

--- model/Opinion.php
+++ model/Opinion.php
@@ -12,4 +12,30 @@
     private $topicId;
     private $userId;
     private $opinionStateId;
+
+    /**
+     * Load data from the database based on this instance's id property
+     * @return bool true on success, false otherwise
+     */
+    public function load() {
+        if($this->id == null)
+            return false;
+
+        $record = Db::selectOneRecord("SELECT description,topic_id,user_id,opinionstate_id  FROM `opinions` WHERE `id`=:id", ["id" => $this->id]);
+        if($record) {
+            $this->description = $record["description"];
+            $this->topicId = $record["topic_id"];
+            $this->userId = $record["user_id"];
+            $this->opinionStateId = $record["opinionstate_id"];
+            return true;
+        }
+        else {
+            $this->description = null;
+            $this->topicId = null;
+            $this->userId = null;
+            $this->opinionStateId = null;
+
+            return false;
+        }
+    }
  }
