8eb1c756be69f8883e0940991784dd0ef57cf641
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-11-10 11:30:21 +0100

    create the Opinion Controller

---
+++ controller/OpinionController.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('model/Opinion.php');
+
+class OpinionController{
+    /**
+     * This method load the opinions from the model and call the index page of opinions view
+     */
+    public index(){
+
+    }
+}
