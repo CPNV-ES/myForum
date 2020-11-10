92f9b73cd0a04b841e2f915671c471f89c3029b8
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-11-10 11:16:59 +0100

    Working display all opinions with user and state

--- controller/ModerationController.php
+++ controller/ModerationController.php
@@ -1,9 +1,20 @@
 <?php 
 
+include "model/Opinion.php";
+include "model/User.php";
+include "model/State.php";
+include "./view/helpers/helper.php";
 class ModerationController
 {
     public function index()
     {
+        $opinions = Opinion::all();
+        $stateType = State::all();
+        foreach($opinions as $Key => $Value)
+        {
+            $Value->username = User::getUsernameById($Value->user_id);
+            $Value->state_name = State::getStateById($Value->opinionstate_id);
+        }
         require_once "./view/moderation/index.view.php";       
     }
 }
--- view/moderation/index.view.php
+++ view/moderation/index.view.php
@@ -1,7 +1,49 @@
 <?php ob_start(); ?>
 
-<h1 class="text-center p-5">Moderation</h1>
+<h1 class="text-center p-5">Opinions</h1>
 
+<table id="references" class="table">
+    <thead>
+        <tr>
+            <th>
+                <h4>Utilisateur</h4>
+            </th>
+            <th>
+                <h4>Opinion</h4>
+            </th>
+            <th>
+                <h4>filtre</h4>
+                <select>
+                    <option>--Tout--</option>
+                    <?php foreach ($stateType as $Key => $Value) : ?>
+                        <option><?=$Value->name?></option>
+                    <?php endforeach; ?>
+                </select>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+
+        foreach ($opinions as $Key => $Value) : ?>
+
+            <tr>
+                <td>
+                    <?= $Value->username ?>
+                </td>
+                <td><?= $Value->description ?></td>
+                <td>
+                    <?= $Value->state_name ?>
+                </td>
+            </tr>
+
+
+
+
+
+        <?php endforeach; ?>
+    </tbody>
+</table>
 
 
 
