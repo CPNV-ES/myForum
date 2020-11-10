574c1aa2a7c343b182786a37c99d353ec0feac46
Quentin Aellen <quentin.aellen@cpnv.ch>
2020-11-10 11:57:20 +0100

    add the display of the data in a table

--- view/opinions/index.view.php
+++ view/opinions/index.view.php
@@ -1,7 +1,30 @@
-<?php ob_start(); ?>
+<?php ob_start();?>
 <h1 class="text-center">Opinions</h1>
 
-
+<div class="row">
+    <table class="table">
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+                <th>vide</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach($opinions as $opinion) :
+                    ?>
+                    <tr>
+                        <td><?= $opinion->pseudo ?></td>
+                        <td><?= $opinion->description ?></td>
+                        <td><?= $opinion->opinionState ?></td>
+                    </tr>
+                    <?php
+                endforeach;
+            ?>
+        </tbody>
+    <table>
+</div>
 <?php
 $content = ob_get_clean();
 require_once $_SERVER['DOCUMENT_ROOT'] . "/view/layout.view.php";
