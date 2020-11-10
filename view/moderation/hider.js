54e46fd5dae7efb5b67dc3de59b09b307fec16b9
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-11-10 12:14:08 +0100

    Create HideContent_Debug

--- view/moderation/hider.js
+++ view/moderation/hider.js
@@ -8,3 +8,27 @@ function HideContent(){
         test[i].style.display = "none";
     }
 }
+
+//Normaly we should use this class, we it actually in works in progress
+function HideContent_debug(){
+    //Set items into the list
+    var items = ["Censuré", "Clos", "En discussion", "Proposé", "Publié"];
+
+    //Parse the list
+    for(var i = 0; i < items.length; i++){
+        //If the item is equal to the selected item
+        if (items[i] == document.getElementById('states').value)
+        {
+            //Display it
+            //console.log(items[i]);
+            console.log(items[i]);
+
+        }else{
+            //hide the rest of the content
+            for(var i2 = 0; i2 < test.length; i2++){
+                test[i].style.display = "none";
+            }
+        }
+        //var content = document.getElementsByClassName(document.getElementById(i).value);
+    }
+}
