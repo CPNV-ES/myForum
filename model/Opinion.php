71f1c553111ae8e47b21edc5a62b48cee6b50d00
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2020-11-10 10:26:31 +0100

    Have an opinions model

--- model/Opinion.php
+++ model/Opinion.php
@@ -1,7 +1,23 @@
 <?php
 
 
-class Moderation
+class Opinion
 {
+    public $id;
+    public $description;
+    public $topic_id;
+    public $user_id;
+    public $opinionstate_id;
 
+    public function __construct() {
+
+    }
+
+    /**
+     * Returns an array of objects representing all records of the table
+     */
+    public static function all()
+    {
+        return Db::selectMany("SELECT * FROM `opinions`", [], "Opinion");
+    }
 }
